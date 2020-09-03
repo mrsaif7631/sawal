e9f493229ddc8aa6741050d73dcb6f434e026551
unknown <saifalam763155@gmail.com>
2020-09-04 00:21:58 +0530

    bbb

---
+++ site/index.php
@@ -0,0 +1,20 @@
+<?php
+<DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Form</title>
+</head>
+<body>
+<!--Form element -->
+<form  action="/sawal/site/form1.html">
+<label for="uid">UID:</label>
+<input type="text" name="uid"><br><br>
+<label for="pwd">PWD:</label>
+<input type="password" name="pwd"><br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
+?>
+
